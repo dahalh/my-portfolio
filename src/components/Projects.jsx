a101327e79f7823e59987419057efaf9af7c7d00
dahalh <dahalh@outlook.com>
2022-11-29 15:56:27 +1100

    new projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Calculator from "../assets/calculator.png";
-import Contact from "../assets/contact.png";
-import Expenses from "../assets/expensestracker.jpg";
-import Movie from "../assets/movie-search.jpg";
-import NotToDo from "../assets/not-to-do.png";
-import Blog from "../assets/blog.jpg";
+// import Calculator from "../assets/calculator.png";
+// import Contact from "../assets/contact.png";
+// import Expenses from "../assets/expensestracker.jpg";
+// import Movie from "../assets/movie-search.jpg";
+// import NotToDo from "../assets/not-to-do.png";
+// import Blog from "../assets/blog.jpg";
+import { ProjectList } from "../data/ProjectList";
 
 const Projects = () => {
   return (
@@ -21,188 +22,33 @@ const Projects = () => {
 
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div
-            style={{ backgroundImage: `url(${Expenses})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Expenses Tracker App
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://fierce-headland-56506.herokuapp.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/dahalh/expenses-tracker/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+          {ProjectList.map((project, id) => {
+            return (
+              <div
+                key={id}
+                style={{ backgroundImage: `url(${project.image})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+              >
+                <div className="opacity-0 group-hover:opacity-100">
+                  <span className="text-2xl font-bold text-white tracking-wider">
+                    {project.name}
+                  </span>
+                  <div className="pt-8 text-center">
+                    <a href={project.demo} target="_blank" rel="noreferrer">
+                      <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                        Demo
+                      </button>
+                    </a>
+                    <a href={project.code} target="_blank" rel="noreferrer">
+                      <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                        Code
+                      </button>
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${Movie})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Movie Search
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://movie-list-p1ijkvpvq-dahalh.vercel.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/dahalh/movie-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${Contact})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Contact List
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://react-contact-list-omega.vercel.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/dahalh/react-contact-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${NotToDo})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Not To Do List
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://dahalh.github.io/time-management/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/dahalh/time-management"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${Calculator})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Prank Calculator
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://dahalh.github.io/prank-calculator"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/dahalh/prank-calculator"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${Blog})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Blog
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://hblog.onrender.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/dahalh/blog-frontend"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/data/ProjectList.js
@@ -0,0 +1,45 @@
+import Expenses from "../assets/expensestracker.jpg";
+import Movie from "../assets/movie-search.jpg";
+import NotToDo from "../assets/not-to-do.png";
+import Blog from "../assets/blog.jpg";
+import CMS from "../assets/CMS.jpg";
+import Notes from "../assets/techNotes.jpg";
+
+export const ProjectList = [
+  {
+    name: "E-Commerce CMS",
+    image: CMS,
+    demo: "https://hd-ecommerce.onrender.com/",
+    code: "https://github.com/dahalh/ecommerce-frontend",
+  },
+  {
+    name: "Ticketing System",
+    image: Notes,
+    demo: "https://htechnotes.onrender.com/",
+    code: "https://github.com/dahalh/ticket-node-frontend",
+  },
+  {
+    name: "Blog",
+    image: Blog,
+    demo: "https://hblog.onrender.com/",
+    code: "https://github.com/dahalh/blog-frontend",
+  },
+  {
+    name: "Expenses Tracker App",
+    image: Expenses,
+    demo: "https://fierce-headland-56506.herokuapp.com/",
+    code: "https://github.com/dahalh/expenses-tracker/",
+  },
+  {
+    name: "Movie Search",
+    image: Movie,
+    demo: "https://movie-list-p1ijkvpvq-dahalh.vercel.app/",
+    code: "https://github.com/dahalh/movie-list",
+  },
+  {
+    name: "Not To Do List",
+    image: NotToDo,
+    demo: "https://dahalh.github.io/time-management/",
+    code: "https://github.com/dahalh/time-management",
+  },
+];
